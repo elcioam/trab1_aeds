1e3d47e10947849e63258fcc06cd82f6f28f3590
elcioamorim <elcioamorim12@gmail.com>
2024-04-12 23:38:31 -0300

    cpp e hpp

---
+++ src/minmax.hpp
@@ -0,0 +1,11 @@
+#ifndef MINMAX_HPP
+#define MINMAX_HPP
+
+#include <vector>
+
+double minmax1(const std::vector<int> vetor);
+double minmax2(const std::vector<int> vetor);
+double minmax3(const std::vector<int> vetor);
+void trabalho();
+
+#endif
